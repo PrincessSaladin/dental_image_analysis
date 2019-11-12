a7877f4821a48f62525ffa655d3f296dbb45831d
Qin Liu <qinliu19@denton.med.unc.edu>
2019-11-12 17:13:52 -0500

    Add the training pipeline

---
+++ segmentation2/vseg_train.py
@@ -0,0 +1,20 @@
+import argparse
+import os
+
+from md_segmentation3d.vseg_train import train
+
+
+def main():
+
+  os.environ['CUDA_VISIBLE_DEVICES'] = '2, 3, 4, 5'
+  
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i', '--input', nargs='?',
+                      default='/home/qinliu19/projects/dental_image_analysis/segmentation2/config/config.py',
+                      help='volumetric segmentation3d train config file')
+  args = parser.parse_args()
+  train(args.input)
+
+
+if __name__ == '__main__':
+  main()
