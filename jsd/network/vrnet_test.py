eaeba09520018b1d54d55551577e2c5681e25979
qinliu <qinliu19@email.unc.edu>
2020-04-05 18:40:24 -0400

    Fix a bug.

--- jsd/network/vrnet_test.py
+++ jsd/network/vrnet_test.py
@@ -4,7 +4,7 @@ from jsd.network.vrnet import RegressionNet
 
 
 def test_vrnet():
-  batch_size, in_channel, dim_z, dim_y, dim_x = 2, 1, 64, 64, 64
+  batch_size, in_channel, dim_z, dim_y, dim_x = 2, 1, 64, 32, 32
   input_tensor = torch.randn([batch_size, in_channel, dim_z, dim_y, dim_x])
   
   input_channel, output_channel = 1, 276
