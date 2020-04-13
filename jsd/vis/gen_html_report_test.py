902ba80f038c4366b57b9cd22345ac84facf6372
qinliu <qinliu19@email.unc.edu>
2020-04-13 18:11:23 -0400

    Add a summary html report

--- jsd/vis/gen_html_report_test.py
+++ jsd/vis/gen_html_report_test.py
@@ -8,12 +8,12 @@ class TestGenHtmlReport(unittest.TestCase):
     self.number = 1
 
   def test_add_document_text(self):
-    res = AddDocumentText(self.string, self.string)
+    res = add_document_text(self.string, self.string)
     self.assertIsNotNone(res)
 
   # WriteToHtmlReportFile(document_text, analysis_text, html_report_path, width)
   def test_write_to_html_report_file(self):
-    res = WriteToHtmlReportFile(self.string, self.string, self.string, self.number)
+    res = write_html_report_for_single_landmark(self.string, self.string, self.string, self.number)
     self.assertIsNone(res)
 
 if __name__ == '__main__':
