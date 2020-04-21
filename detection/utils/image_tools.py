69e66679b888df4aa622b4b74aa6863ddbee1563
Qin Liu <sjtuliuqin@163.com>
2020-04-21 12:44:30 -0400

    Fix a bug

--- detection/utils/image_tools.py
+++ detection/utils/image_tools.py
@@ -15,7 +15,7 @@ def weighted_voxel_center(image, threshold_min, threshold_max):
     image_npy[image_npy > threshold_max] = 0
     weight_sum = np.sum(image_npy)
     if weight_sum <= 0:
-        return None
+        return [weight_sum, weight_sum, weight_sum]
 
     image_npy_x = np.zeros_like(image_npy)
     for i in range(image_npy.shape[0]):
@@ -29,9 +29,9 @@ def weighted_voxel_center(image, threshold_min, threshold_max):
     for k in range(image_npy.shape[2]):
         image_npy_z[:, :, k] = k
 
-    weighted_center_x = np.multiply(image_npy, image_npy_x) / weight_sum
-    weighted_center_y = np.multiply(image_npy, image_npy_y) / weight_sum
-    weighted_center_z = np.multiply(image_npy, image_npy_z) / weight_sum
+    weighted_center_x = np.sum(np.multiply(image_npy, image_npy_x)) / weight_sum
+    weighted_center_y = np.sum(np.multiply(image_npy, image_npy_y)) / weight_sum
+    weighted_center_z = np.sum(np.multiply(image_npy, image_npy_z)) / weight_sum
     weighted_center = [weighted_center_z, weighted_center_y, weighted_center_x]
 
     return weighted_center
