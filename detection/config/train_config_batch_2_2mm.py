bb5f994ccc45e24049a93995d2ec8d63cb4569bd
Qin Liu <sjtuliuqin@163.com>
2020-05-02 18:13:22 -0400

    Modify configs to train on the server

--- detection/config/train_config_batch_2_2mm.py
+++ detection/config/train_config_batch_2_2mm.py
@@ -9,7 +9,7 @@ cfg = __C
 ##################################
 __C.general = {}
 
-__C.general.training_image_list_file = '/mnt/projects/CT_Dental/dataset/landmark_detection/train_local.csv'
+__C.general.training_image_list_file = '/shenlab/lab_stor6/projects/CT_Dental/dataset/landmark_detection/train_server.csv'
 
 __C.general.validation_image_list_file = ''
 
@@ -46,11 +46,11 @@ __C.general.target_organ_label = {
     'mandible': 2
 }
 
-__C.general.save_dir = '/mnt/projects/CT_Dental/models/model_0419_2020'
+__C.general.save_dir = '/shenlab/lab_stor6/qinliu/projects/CT_Dental/models/model_0502_2020/batch_2'
 
 __C.general.resume_epoch = -1
 
-__C.general.num_gpus = 0
+__C.general.num_gpus = 1
 
 ##################################
 # dataset parameters
@@ -59,7 +59,7 @@ __C.dataset = {}
 
 __C.dataset.crop_spacing = [2, 2, 2]      # mm
 
-__C.dataset.crop_size = [48, 48, 48]   # voxel
+__C.dataset.crop_size = [96, 96, 96]   # voxel
 
 __C.dataset.sampling_size = [6, 6, 6]      # voxel
 
@@ -67,9 +67,9 @@ __C.dataset.positive_upper_bound = 3    # voxel
 
 __C.dataset.negative_lower_bound = 6    # voxel
 
-__C.dataset.num_pos_patches_per_image = 3
+__C.dataset.num_pos_patches_per_image = 8
 
-__C.dataset.num_neg_patches_per_image = 9
+__C.dataset.num_neg_patches_per_image = 16
 
 # crop intensity normalizers (to [-1,1])
 # one normalizer corresponds to one input modality
@@ -82,7 +82,7 @@ __C.dataset.crop_normalizers = [AdaptiveNormalizer()]
 # 2) MASK: sampling crops randomly within segmentation mask
 # 3) HYBRID: Sampling crops randomly with both GLOBAL and MASK methods
 # 4) CENTER: sampling crops in the image center
-__C.dataset.sampling_method = 'HYBRID'
+__C.dataset.sampling_method = 'GLOBAL'
 
 # linear interpolation method:
 # 1) NN: nearest neighbor interpolation
@@ -109,7 +109,7 @@ __C.landmark_loss = {}
 
 __C.landmark_loss.name = 'Focal'          # 'Dice', or 'Focal'
 
-__C.landmark_loss.focal_obj_alpha = [0.75] * 14  # class balancing weight for focal loss
+__C.landmark_loss.focal_obj_alpha = [0.75] * 24  # class balancing weight for focal loss
 
 __C.landmark_loss.focal_gamma = 2         # gamma in pow(1-p,gamma) for focal loss
 
@@ -125,17 +125,17 @@ __C.net.name = 'vdnet'
 ##################################
 __C.train = {}
 
-__C.train.epochs = 101
+__C.train.epochs = 2001
 
-__C.train.batch_size = 1
+__C.train.batch_size = 4
 
-__C.train.num_threads = 1
+__C.train.num_threads = 4
 
 __C.train.lr = 1e-4
 
 __C.train.betas = (0.9, 0.999)
 
-__C.train.save_epochs = 10
+__C.train.save_epochs = 100
 
 ##################################
 # debug parameters
