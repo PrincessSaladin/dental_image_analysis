49d78c43da58361003400e3a6e7ca437ab928cec
Qin Liu <qinliu19@denton.med.unc.edu>
2020-05-03 12:41:45 -0400

    Modify the path

--- detection/det_infer.py
+++ detection/det_infer.py
@@ -294,11 +294,11 @@ def main():
                        '2. A text file containing paths of all testing images\n' \
                        '3. A folder containing all testing images\n'
 
-    default_input = '/shenlab/lab_stor6/projects/CT_Dental/dataset/landmark_detection/test_server.csv'
-    default_model = '/shenlab/lab_stor6/qinliu/projects/CT_Dental/models/model_0421_2020'
-    default_output = '/shenlab/lab_stor6/qinliu/projects/CT_Dental/results/model_0421_2020/test_set'
+    default_input = '/shenlab/lab_stor6/projects/CT_Dental/dataset/landmark_detection/test_1_server.csv'
+    default_model = '/shenlab/lab_stor6/qinliu/projects/CT_Dental/models/model_0502_2020/batch_1'
+    default_output = '/shenlab/lab_stor6/qinliu/projects/CT_Dental/results/model_0502_2020/batch_1/test_set/'
     default_save_prob = False
-    default_gpu_id = 6
+    default_gpu_id = 5
 
     parser = argparse.ArgumentParser(description=long_description)
     parser.add_argument('-i', '--input', default=default_input,
